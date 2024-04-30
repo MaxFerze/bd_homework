dd3d20d6e6994f6d5f595f39b64a67e51204de28
Max_Ferze <maksbyr912@gmail.com>
2024-04-30 13:13:51 +0300

    fix

--- insert.sql
+++ insert.sql
@@ -1,128 +1,68 @@
 insert into genre(genre)
-values ('pop');
-
-insert into genre(genre)
-values ('metal');
-
-insert into genre(genre)
-values ('electronic');
+values
+	('pop'),
+	('metal'),
+	('electronic');
 
 insert into performer(performer)
-values ('The Living Tombstone');
-
-insert into performer(performer)
-values ('MiatriSs');
-
-insert into performer(performer)
-values ('T.a.T.u.');
-
-insert into performer(performer)
-values ('imagine dragons');
+values
+	('The Living Tombstone'),
+	('MiatriSs'),
+	('T.a.T.u.'),
+	('imagine dragons');
 
 insert into perfomer_ganre(performer_id, genre_id)
-values ('1', '2');
-
-insert into perfomer_ganre(performer_id, genre_id)
-values ('2', '2');
-
-insert into perfomer_ganre(performer_id, genre_id)
-values ('3', '1');
-
-insert into perfomer_ganre(performer_id, genre_id)
-values ('4', '5');
+values
+	('1', '2'),
+	('2', '2'),
+	('3', '1'),
+	('4', '5');
 
 insert into album(album, release_data)
-values ('RadioBunny Ink', '2016.10.20');
-
-insert into album(album, release_data)
-values ('200 по встречной', '2001.05.21');
-
-insert into album(album, release_data)
-values ('Night Visions', '2012.09.04');
-
-insert into album(album, release_data)
-values ('My Ordinary Life', '2017.11.29');
-
-insert into album(album, release_data)
-values ('Fly', '2019.10.10')
+values
+	('RadioBunny Ink', '2016.10.20'),
+	('200 по встречной', '2001.05.21'),
+	('Night Visions', '2012.09.04'),
+	('My Ordinary Life', '2017.11.29'),
+	('Fly', '2019.10.10');
 
 insert into album_author (performer_id, album_id)
-values ('2', '1');
-
-insert into album_author (performer_id, album_id)
-values ('3', '2');
-
-insert into album_author (performer_id, album_id)
-values ('4', '3');
-
-insert into album_author (performer_id, album_id)
-values ('1', '4');
-
-insert into album_author (performer_id, album_id)
-values ('1', '5')
+values
+	('2', '1'),
+	('3', '2'),
+	('4', '3'),
+	('1', '4'),
+	('1', '5');
 
 insert into song(name_song, album, duration)
-values ('Нас не догонят', '2', '00:03:57');
-
-insert into song(name_song, album, duration)
-values ('White robe', '2', '00:03:36');
-
-insert into song(name_song, album, duration)
-values ('Take me', '1', '00:04:18');
-
-insert into song(name_song, album, duration)
-values ('Доброта не в моде', '1', '00:04:57');
-
-insert into song(name_song, album, duration)
-values ('Radioactive', '3', '00:03:07');
-
-insert into song(name_song, album, duration)
-values ('Demons', '3', '00:02:57');
-
-insert into song(name_song, album, duration)
-values ('My Ordinary Life', '3', '00:04:05');
-
-insert into song(name_song, album, duration)
-values ('ECHO Crusher-P', '5', '00:03:23')
+values
+	('Нас не догонят', '2', '00:03:57'),
+	('White robe', '2', '00:03:36'),
+	('Take me', '1', '00:04:18'),
+	('Доброта не в моде', '1', '00:04:57'),
+	('Radioactive', '3', '00:03:07'),
+	('Demons', '3', '00:02:57'),
+	('My Ordinary Life', '3', '00:04:05'),
+	('ECHO Crusher-P', '5', '00:03:23');
 
 insert into collection(collection, release_data)
-values ('Rus songs', '2012.09.04');
-
-insert into collection(collection, release_data)
-values ('gold', '2012.09.04');
-
-insert into collection(collection, release_data)
-values ('my collection', '2012.09.04');
+values
+	('Rus songs', '2012.09.04'),
+	('gold', '2012.09.04'),
+	('my collection', '2012.09.04');
 
 insert into song_in_collection(song_id, collection_id)
-values ('1', '1');
-
-insert into song_in_collection(song_id, collection_id)
-values ('4', '1');
-
-insert into song_in_collection(song_id, collection_id)
-values ('1', '2');
-
-insert into song_in_collection(song_id, collection_id)
-values ('2', '2');
-
-insert into song_in_collection(song_id, collection_id)
-values ('5', '2');
-
-insert into song_in_collection(song_id, collection_id)
-values ('1', '3');
-
-insert into song_in_collection(song_id, collection_id)
-values ('2', '3');
-
-insert into song_in_collection(song_id, collection_id)
-values ('4', '3');
-
-insert into song_in_collection(song_id, collection_id)
-values ('5', '3');
-
-insert into song_in_collection(song_id, collection_id)
-values ('6', '3');
+values
+	('1', '1'),
+	('4', '1'),
+	('1', '2'),
+	('2', '2'),
+	('5', '2'),
+	('1', '3'),
+	('2', '3'),
+	('4', '3'),
+	('5', '3'),
+	('6', '3');
 
 update collection
 set release_data = '2018.12.08'
--- select.sql
+++ select.sql
@@ -1,7 +1,7 @@
 -- 2 задание
-select name_song, duration from song
-where duration = (select max(duration) from song)
-limit 1;
+select name_song, max(duration) from song
+group by name_song
+order by max(duration) desc
 
 select name_song, duration from song
 where duration > '00:03:30'
@@ -20,22 +20,31 @@ where lower(name_song) like '%my%' or lower(name_song) like '%мой%'
 select count(performer_id), genre_id from perfomer_ganre pg
 group by genre_id
 
-select count(*) from song
-where album = (select id from album where release_data between '2019-01-01' and '2020-01-01')
+select count(*) from song s
+join album a on s.id = a.id
+where a.release_data between '2019-01-01' and '2020-01-01'
 
 select album, avg(duration) from song
 group by album
 
-select performer from performer
-where id in (select performer_id from album_author where album_id not in (select id from album where release_data between '2020-01-01' and '2021-01-01'))
+select distinct performer from performer p
+join album_author aa on p.id = aa.performer_id
+where album_id not in (select id from album where release_data between '2020-01-01' and '2021-01-01')
 
-select collection from collection
-where id in (select collection_id from song_in_collection where song_id in (select id from song where album in (select album_id from album_author where performer_id = 2)))
+select collection from collection c
+join song_in_collection sic on c.id = sic.collection_id
+join song s on sic.song_id = s.id
+join album_author aa on s.album = aa.album_id
+where performer_id = 2
+
+
 
 
 -- 4 задание, необязательное, не все
 select id, name_song from song
 where id not in (select song_id from song_in_collection)
 
-select performer from performer
-where id = (select performer_id from album_author where album_id = (select album from song where duration = (select min(duration) from song)))
+select performer from performer p
+join album_author aa on p.id = aa.performer_id
+join song s on aa.album_id = s.album
+where duration = (select min(duration) from song)
